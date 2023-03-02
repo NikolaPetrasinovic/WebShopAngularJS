9ffb5e6d7ef130184f8d04ee2fa52e502e2e6ded
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-03-02 14:29:08 +0100

    asd

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
-var app = angular
-            .module("Demo", ["ngRoute"])
-            .config(function ($routeProvider) {
-                $routeProvider
-                    .when("/home", {
-                        templateUrl: "./templates/home.html",
-                        controller: "homeController"
+var app = angular.module("Demo", ["ngRoute"])
+                 .config(function($routeProvider, $locationProvider){
+                    $routeProvider
+                        .when("/home", {
+                            templateUrl:"./templates/home.html",
+                            controller:"homeController as homeCtrl"
+                        })
+                        .when("/courses", {
+                            templateUrl:"./templates/courses.html",
+                            controller:"coursesController as coursesCtrl"
+                        })
+                        .when("/students", {
+                            templateUrl:"./templates/students.html",
+                            controller:"studentsController as studentsCtrl"
+                        })
+                        .when("/students/:id", {
+                            templateUrl:"./templates/studentDetails.html",
+                            controller:"studentDetailsController as studentDetailsCtrl" 
+                        })
+                        .otherwise({
+                            redirectTo: "/home"
+                        })
+                    $locationProvider.html5Mode(true);
+                 })
+                 .controller("homeController", function(){
+                    this.message = "Home Page";
+                 })
+                 .controller("studentsController", function($http){
+                    this.message = "Students Page";
+                    var vm = this;
+                    $http.get('http://localhost:3000/Students')
+                         .then(function (response){
+                            vm.students = response.data;
+                         })
+                 })
+                 .controller("coursesController", function($http){
+                    this.message = ["C#", "SQL", "PHP", "C++"];
+                    var vm = this;
+                    $http.get('http://localhost:3000/courses')
+                         .then(function (response){
+                            vm.courses = response.data
+                         })
+                 })
+                 .controller("studentDetailsController", function($http, $routeParams){
+                    var vm = this;
+                    vm.students = [];
+                    $http({
+                        url:"http://localhost:3000/Students",
+                        params:{id:$routeParams.id},
+                        method:"get"
                     })
-                    .when("/courses", {
-                        templateUrl: "./templates/courses.html",
-                        controller: "coursesController"
+                    .then(function(response){
+                        
+                        vm.students = response.data
                     })
-                    .when("/students", {
-                        templateUrl: "./templates/students.html",
-                        controller: "studentsController"
-                    })
-            })
-            .controller("homeController", function ($scope) {
-                $scope.message = "Home Page";
-            })
-            .controller("coursesController", function ($scope) {
-                $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server", "AngularJS", "JavaScript"];
-            })
-             .controller("studentsController", function ($scope, $http) {
-                 $http.get("http://localhost:3000/Students")
-                                        .then(function (response) {
-                                            $scope.students = response.data;
-                                        })
-             })
+                 })
