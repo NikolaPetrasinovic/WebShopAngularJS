81edb07108c40edfec357fa0c5b81714839abd3c
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-03-13 12:45:26 +0100

    created route for productDetails

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-var app = angular.module("shoppingcart", [])
-		.controller("shoppingCTR", function($scope){
+var app = angular.module("shoppingcart", ["ui.router"])
+	.config(function($stateProvider) {
+	$stateProvider
+		.state("ProductDetails", {
+			url: "/productDetails",
+			templateUrl: "./templates/productDetails.html",
+			controller: "productDetailsController",
+			controllerAs: "productDetailsCtrl"
+		})	
+		
+	})
+
+.controller("shoppingCTR", function($scope){
 			$scope.carts=[];
 			$scope.products = [
 				{p_id : "1", p_name: "Nike AirMax 270", p_image: "img/1.jpg", p_price: 500},
@@ -13,5 +24,15 @@ var app = angular.module("shoppingcart", [])
 				$scope.carts.push({p_id: product.p_id, p_name: product.p_name, p_price: product.p_price});
 			}
 		}
-
+		
 	})
+
+	// .controller("productDetailsController", function($scope, $routeParams){
+	// 	var productId = $routeParams.id;
+	// 	angular.forEach($scope.products, function(product) {
+	// 		if (product.id === productId) {
+	// 		  $scope.selectedProduct = product;
+	// 		}
+	// 	})
+	// })
+
