171a81c5058ad6e29c0793d676bf0619fe3784b3
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-03-24 11:43:47 +0100

    adjusted code, using json api instead of localStor

--- script.js
+++ script.js
@@ -21,146 +21,140 @@ var app = angular.module("webshop", ["ui.router"])
 	
 .controller("homeController", homeController)
 .controller("productDetailsController", productDetails)
-.factory("CartService", cartService)
-
-	function cartService ($window) {
-		var cartData = [];
+.factory("CartService", CartService)
   
+function CartService($http) {
+    var cartData = [];
+
     function getCartData() {
-      var cartDataJSON = $window.localStorage.getItem("cart");
-      if (cartDataJSON) {
-        cartData = JSON.parse(cartDataJSON);
-      }
       return cartData;
     }
   
     function saveCartData(cart) {
-      $window.localStorage.setItem("cart", JSON.stringify(cart));
+      $http.post("http://localhost:3000/cart", cart)
+      .then(function(response) {
+      	cartData = response.data;
+      });
     }
   
     return {
       getCartData: getCartData,
       saveCartData: saveCartData,
     };
-	}
+}
+  
 
-	function homeController($http, $state, CartService) {
-		var vm = this;
-		vm.searchTerm = ""; // searchTerm for searching products
-		vm.cartIsOpen = false;
-		vm.toggleCart = toggleCart;
-		vm.addtoCart = addtoCart;
-		vm.cartTotal = cartTotal;
-		vm.removeProduct = removeProduct;
-		vm.buyOrder = buyOrder;
-		vm.filterProducts = filterProducts;
-	  
-		function toggleCart() {
-		  vm.cartIsOpen = !vm.cartIsOpen;
-		}
-	  
-		vm.cart = CartService.getCartData(); // ucitavanje korpe iz localstorage
-	  
-		$http.get("http://localhost:3000/products").then(function (response) {
-		  vm.products = response.data;
-		  console.log(vm.products);
-		});
-	  
-		function addtoCart(product) {
-		  var existingProduct = vm.cart.find(function (item) {
-			return item.id === product.id;
-		  });
-		  if (existingProduct) {
-			existingProduct.quantity++;
-		  } else {
-			product.quantity = 1;
-			vm.cart.push(product);
-		  }
-		  CartService.saveCartData(vm.cart); // cuvanje update korpe u local
-		}
-	  
-		function cartTotal() {
-		  var totalPrice = 0;
-		  angular.forEach(vm.cart, function (product) {
-			totalPrice += product.price * product.quantity;
-		  });
-		  return totalPrice;
-		}
-	  
-		function removeProduct(product) {
-		  var index = vm.cart.indexOf(product);
-		  vm.cart.splice(index, 1);
-		  CartService.saveCartData(vm.cart); // cuvanje update korpe u local
-		}
-	  
-		function buyOrder() {
-		  var orderProducts = [];
-	  
-		  angular.forEach(vm.cart, function (product) {
-			orderProducts.push({
-			  id: product.id,
-			  name: product.name,
-			  price: product.price,
-			  quantity: product.quantity,
-			});
-		  });
-	  
-		  var order = {
-			products: orderProducts,
-			total: vm.cartTotal(),
-		  };
-	  
-		  $http.post("http://localhost:3000/orders", order).then(function (response) {
-			alert("Order successful!");
-		  });
-		  vm.cart = [];
-		  CartService.saveCartData(vm.cart); // cuvanje prazne korpe u localstorage
-		}
-	  
-		function filterProducts(product) {
-		  return (
-			vm.searchTerm == "" ||
-			product.name.toLowerCase().indexOf(vm.searchTerm.toLowerCase()) !== -1
-		  );
-		}
-	  }
+function homeController($http, CartService) {
+  var vm = this;
+  vm.searchTerm = ""; // searchTerm for searching products
+  vm.cartIsOpen = false;
+  vm.toggleCart = toggleCart;
+  vm.addtoCart = addtoCart;
+  vm.cartTotal = cartTotal;
+  vm.removeProduct = removeProduct;
+  vm.buyOrder = buyOrder;
+  vm.filterProducts = filterProducts;
+
+  function toggleCart() {
+    vm.cartIsOpen = !vm.cartIsOpen;
+  }
+
+  vm.cart = CartService.getCartData(); 
+
+  $http.get("http://localhost:3000/products")
+  .then(function (response) {
+    vm.products = response.data;
+    console.log(vm.products);
+  });
+
+  function addtoCart(product) {
+    var existingProduct = vm.cart.find(function (item) {
+      return item.id === product.id;
+    });
+    if (existingProduct) {
+      existingProduct.quantity++;
+    } else {
+      product.quantity = 1;
+      vm.cart.push(product);
+    }
+    CartService.saveCartData(vm.cart);
+  }
+
+  function cartTotal() {
+    var totalPrice = 0;
+    angular.forEach(vm.cart, function (product) {
+      totalPrice += product.price * product.quantity;
+    });
+    return totalPrice;
+  }
+
+  function removeProduct(product) {
+    var index = vm.cart.indexOf(product);
+    vm.cart.splice(index, 1);
+    CartService.saveCartData(vm.cart); 
+  }
+
+  function buyOrder() {
+    var orderProducts = [];
+
+    angular.forEach(vm.cart, function (product) {
+      orderProducts.push({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        quantity: product.quantity,
+      });
+    });
+
+    var order = {
+      products: orderProducts,
+      total: vm.cartTotal(),
+    };
+
+    $http.post("http://localhost:3000/orders", order)
+    .then(function (response) {
+      alert("Order successful!");
+    });
+    vm.cart = [];
+    CartService.saveCartData(vm.cart);
+  }
+
+  function filterProducts(product) {
+    return (
+      vm.searchTerm == "" ||
+      product.name.toLowerCase().indexOf(vm.searchTerm.toLowerCase()) !== -1
+    );
+  }
+}
 	
-	function productDetails($http, $stateParams, $window){
-		var vm = this;
+function productDetails($http, $stateParams, CartService) {
+  var vm = this;
+  vm.initCart = function () {
+      vm.cart = CartService.getCartData();
+  };
 
-		$http({
-			url:"http://localhost:3000/products",
-			params:{id:$stateParams.id},
-			method:"get"
-		})
-		.then(function(response){
-			
-			vm.product = response.data[0]
-			console.log(response)
-		})
-		//dodavanje proizvoda u korpu i cuvanje u json
-		vm.addToCart = function (product) {
-			var existingProduct = vm.cart.find(function (item) {
-			  return item.id === product.id;
-			});
-			if (existingProduct) {
-			  existingProduct.quantity++;
-			} else {
-			  product.quantity = 1;
-			  vm.cart.push(product);
-			}
-			//cuvanje u jsonu
-			$window.localStorage.setItem('cart', JSON.stringify(vm.cart));
-			alert("Product added to cart");
-		};
+  $http({
+      url: "http://localhost:3000/products",
+      params: { id: $stateParams.id },
+      method: "get"
+  }).then(function (response) {
+      vm.product = response.data[0];
+  });
 
-		
-		//inicijalizacija korpe iz jsona
-		vm.initCart = function () {
-			vm.cart = JSON.parse($window.localStorage.getItem('cart')) || [];
-		  };
+  vm.addToCart = function (product) {
+      var existingProduct = vm.cart.find(function (item) {
+          return item.id === product.id;
+      });
+      if (existingProduct) {
+          existingProduct.quantity++;
+      } else {
+          product.quantity = 1;
+          vm.cart.push(product);
+      }
+      CartService.saveCartData(vm.cart);
+  };
 
-		vm.initCart();
-	 }
-
+  vm.initCart(); //immediatly after function add to cart, for latest data
+}
+	  
 	
