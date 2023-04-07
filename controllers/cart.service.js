72f58a8a72d98f355289029c308ac3152ea389ae
NikolaPetrasinovic <nikola.petrasinovic@dev.telnet.rs>
2023-04-07 09:54:10 +0200

    solved .then error added funct. return new promise

--- controllers/cart.service.js
+++ controllers/cart.service.js
@@ -7,26 +7,29 @@ function CartService($http) {
 	var cartData = [];
 
 	function getProducts() {
-		return $http.get('http://localhost:3000/products').then(function (response) { 
-			vm.products = response.data;
-			return vm.products;
-		})
-		.catch(function(error) {
-			console.error(error);
-			throw error;
-		});
+		return $http
+			.get('http://localhost:3000/products')
+			.then(function (response) {
+				vm.products = response.data;
+				return vm.products;
+			})
+			.catch(function (error) {
+				console.error(error);
+				throw error;
+			});
 	}
-	
 
 	function getCartData() {
-		return $http.get('http://localhost:3000/cart').then(function (response) {
-			cartData = response.data;
-			return cartData;
-		})
-		.catch(function(error) {
-            console.error(error);
-            throw error;
-        });
+		return $http
+			.get('http://localhost:3000/cart')
+			.then(function (response) {
+				cartData = response.data;
+				return cartData;
+			})
+			.catch(function (error) {
+				console.error(error);
+				throw error;
+			});
 	}
 
 	function saveCartData(cart) {
@@ -40,57 +43,66 @@ function CartService($http) {
 		}
 	}
 	function deleteProduct(id) {
-		return $http.delete('http://localhost:3000/cart/' + id).then(function (deleteResponse) {
-			console.log('Product deleted.');
-		})
-		.catch(function(error) {
-			console.error('Error deleting product:', error);
-			throw error;
-		  });
+		return $http
+			.delete('http://localhost:3000/cart/' + id)
+			.then(function (deleteResponse) {
+				console.log('Product deleted.');
+			})
+			.catch(function (error) {
+				console.error('Error deleting product:', error);
+				throw error;
+			});
 	}
 
 	function updateCart(product) {
-		return $http.put('http://localhost:3000/cart/' + product.id, product)
-		.catch(function(error) {
+		return $http.put('http://localhost:3000/cart/' + product.id, product).catch(function (error) {
 			console.error('Error updating cart:', error);
 			throw error;
-		  });
+		});
 	}
 
 	function addtoCart(product) {
-		if (!Array.isArray(cartData)) {
-		  cartData = [];
-		}
-		var existingProduct = cartData.findIndex(function (item) {
-		  return item.productID === product.id;
-		}); 
-		if (existingProduct === -1) {
-		  var newObject = {
-			productID: product.id,
-			name: product.name,
-			price: product.price,
-			quantity: product.quantity,
-			imageURL: product.image
-		  }; 
-		  var t;
-		  $http.post('http://localhost:3000/cart', newObject).then(function (response) {
-			t = response.data.id;
-			t = {
-			  productID: product.id,
-			  name: product.name,
-			  price: product.price,
-			  quantity: product.quantity,
-			  imageURL: product.image,
-			  id: t
-			};
-			cartData.push(t);
-		  });
-		  console.log(cartData);
-		} else {
-		  cartData[existingProduct].quantity++;
-		  updateCart(cartData[existingProduct]);
-		}
-	  }
+		return new Promise(function(resolve, reject) {
+			if (!Array.isArray(cartData)) {
+				cartData = [];
+			}
+			var existingProduct = cartData.findIndex(function (item) {
+				return item.productID === product.id;
+			}); 
+			if (existingProduct === -1) {
+				var newObject = {
+					productID: product.id,
+					name: product.name,
+					price: product.price,
+					quantity: product.quantity,
+					imageURL: product.image
+				}; 
+				var t;
+				$http.post('http://localhost:3000/cart', newObject).then(function (response) {
+					t = response.data.id;
+					t = {
+						productID: product.id,
+						name: product.name,
+						price: product.price,
+						quantity: product.quantity,
+						imageURL: product.image,
+						id: t
+					};
+					cartData.push(t);
+					resolve(cartData);
+				})
+				.catch(function(error) {
+					console.error('Error adding product to cart:', error);
+					reject(error);
+				});
+			} else {
+				cartData[existingProduct].quantity++;
+				updateCart(cartData[existingProduct]);
+				resolve(cartData);
+			}
+		});
+	}
+	
 	return {
 		getProducts: getProducts,
 		getCartData: getCartData,
@@ -101,7 +113,6 @@ function CartService($http) {
 	};
 }
 
-
-	//   $http.put('http://localhost:3000/cart/' + cartData[existingProduct].id, cartData[existingProduct]).then(function (response) {
-		// 	console.log(cartData[existingProduct].id);
-		//   });
+//   $http.put('http://localhost:3000/cart/' + cartData[existingProduct].id, cartData[existingProduct]).then(function (response) {
+// 	console.log(cartData[existingProduct].id);
+//   });
